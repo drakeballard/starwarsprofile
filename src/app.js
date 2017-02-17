6ef2b603298108ca07324ac6b16c76f914801139
Drake Ballard <drakeballard@gmail.com>
2017-02-16 19:41:28 -0600

    adding the app.js file structure

---
+++ src/app.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from 'components/App.js';
+import { AppContainer } from 'react-hot-loader';
+
+require('index.html');
+
+const container = document.getElementById('#app-container');
+
+ReactDOM.render(
+  <AppContainer>
+    <App />
+  </AppContainer>,
+  container
+)
+
+if(module.hot) {
+  module.hot.accept('./components/App', () => {
+    ReactDOM.render(
+      <AppContainer>
+        <App />
+      </AppContainer>,
+      container
+    );
+  });
+}

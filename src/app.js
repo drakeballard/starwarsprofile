6bbf0c2cf9e1306e2c6e80ee170a48a5106c0724
Drake Ballard <drakeballard@gmail.com>
2017-02-16 20:16:48 -0600

    adding reducer/index.js

--- src/app.js
+++ src/app.js
@@ -2,6 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.js';
 import { AppContainer } from 'react-hot-loader';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+import reducer from './reducer';
+
+const store = createStore(reducer, compose(
+  applyMiddleware(thunk),
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+));
 
 require('./index.html');
 
@@ -9,7 +17,9 @@ const container = document.getElementById('app-container');
 
 ReactDOM.render(
   <AppContainer>
+    <Provider store={store}>
     <App />
+    </Provider>
   </AppContainer>,
   container
 )
